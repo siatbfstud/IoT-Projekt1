e5f0019c3d43a59d4af12720bb8e6240e0f2bafd
Tais <tais0340@stud.kea.dk>
2021-11-10 13:40:47 +0100

    geofence working

---
+++ geofence.py
@@ -0,0 +1,24 @@
+import picket
+import GPSfunk
+
+def testzone(lat, lon):
+    my_fence = picket.Fence()
+
+
+    #Inde i zone
+    my_fence.add_point((55.706677, 12.538627))
+    my_fence.add_point((55.706165, 12.538455))
+    my_fence.add_point((55.705993, 12.540409))
+    my_fence.add_point((55.706570, 12.540522))
+    
+    #Ude af zone
+#     my_fence.add_point((55.707020, 12.537976))
+#     my_fence.add_point((55.707026, 12.537022))
+#     my_fence.add_point((55.707507, 12.537038))
+#     my_fence.add_point((55.707581, 12.538344))
+
+    #assert my_fence.check_point((lat, lon)), "The statement is false :D"
+    if my_fence.check_point((lat, lon)) == True:
+        print("Inde i zonen")
+    else:
+        print("Ude af zonen")
--- gpsfunk.py
+++ gpsfunk.py
@@ -1,20 +1,21 @@
 from machine import UART
 from micropyGPS import MicropyGPS
+import geofence
 
-def main():
+def gps_funk():
     uart = UART(2, baudrate=9600, bits=8, parity=None, stop=1, timeout=5000, rxbuf=1024)
     gps = MicropyGPS()
     while True:
         buf = uart.readline()
 
         for char in buf:
-            gps.update(chr(char)) # Note the conversion to to chr, UART outputs ints normally
+            gps.update(chr(char))
 
         #print('UTC Timestamp:', gps.timestamp)
         #print('Date:', gps.date_string('long'))
         #print('Satellites:', gps.satellites_in_use)
         #print('Altitude:', gps.altitude)
-        #print('Latitude:', gps.latitude_string())
+        #print('latitude:', gps.latitude_string())
         #print('Longitude:', gps.longitude_string())
         #print('Horizontal Dilution of Precision:', gps.hdop)
         
@@ -30,9 +31,13 @@ def main():
         gps_ada = formattedSpd+","+formattedLat+","+formattedLon+","+formattedAlt
         # def startGPSthread():
         # _thread.start_new_thread(main, ())
-        if formattedLat != "0.0":
-            print("gps_ada: ",gps_ada)
+        
+        if gps.latitude[0] != 0.0:
+            #print("gps_ada: ",gps_ada)
+            print('latitude:', gps.latitude[0])
+            print('longitude:', gps.longitude[0])
+            geofence.testzone(gps.latitude[0], gps.longitude[0])
             return gps_ada
-if __name__ == "__main__":
-    print('...running main, GPS testing')
-    main()
+if __name__ == "__gps_funk__":
+    print('...running gps_funk, GPS testing')
+    gps_funk()
